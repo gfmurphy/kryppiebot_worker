2734bb74678f7facc996759166c8b65cb85ae05a
George F. Murphy <gfmurphy@gmail.com>
2014-07-27 17:15:02 -0500

    Initial commit of background worker.

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+ruby "2.1.2"
+
+gem "json", "~> 1.8"
+gem "mocha", "~> 1.1", group: :test
+gem "redis", "~> 3.1"
+
---
+++ worker.rb
@@ -0,0 +1,19 @@
+require "redis"
+require "json"
+require "logger"
+require "uri"
+
+default_redis_url = "redis://localhost"
+
+uri = URI(ENV["REDISTOGO_URL"] || default_redis_url)
+REDIS = Redis.new(url: uri.to_s)
+
+LOGGER = Logger.new($stdout)
+LOGGER.level = Logger.const_get ENV["LOG_LEVEL"] || "ERROR"
+
+REDIS.subscribe("groupme:message") do |on|
+  on.message do |channel, message|
+    data = JSON.parse(message.to_s)
+    LOGGER.debug("Message received: %s" % data.inspect)
+  end
+end

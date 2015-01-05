e62a530d4115de9f6b2a9ed9aafac6f50c9456b6
George F. Murphy <gfmurphy@gmail.com>
2015-01-04 18:47:52 -0600

    Post urls of BFL parent tweets to channel

--- lib/listeners/bfl_parent_comment.rb
+++ lib/listeners/bfl_parent_comment.rb
@@ -1,5 +1,7 @@
 module Listeners
   class BflParentComment
+    include GroupMe
+
     def initialize(user_id, twitter)
       @user_id = user_id
       @twitter = twitter 
@@ -10,8 +12,12 @@ module Listeners
       user_id = message["user_id"].to_s
 
       if @user_id.to_s == user_id && text.gsub(/\W+/, ' ') =~ /as a parent/mi
-        @twitter.tweet(text)
+        tweet = @twitter.tweet(text)
+        post_as_bot(KRYPPIE_BOT_ID, tweet.url)
       end
+    rescue TwitterSupport::Error => e
+      log(:error).error(e)
+      post_as_bot(KRYPPIE_BOT_ID, "This should be tweeted, '#{text}', but I couldn't do it.")
     end
   end
 end
--- test/lib/listeners/bfl_parent_comment_test.rb
+++ test/lib/listeners/bfl_parent_comment_test.rb
@@ -8,12 +8,29 @@ module Listeners
     end
     
     def test_parent_comment_from_bfl
-      text = "foo As a \npaRent i try"
-      message = { "user_id" => @bfl_user_id, "text" => text }
-      @mock_tweeter.expects(:tweet).with(text).once
-      
+      stub_text = "foo As a \npaRent i try"
+      stub_message = { "user_id" => @bfl_user_id, "text" => stub_text }
+      stub_url = "http://twitter.com/tweets/1234"
+      mock_tweet = mock(url: stub_url)
+      @mock_tweeter.expects(:tweet).with(stub_text).once.returns(mock_tweet)
+      comment_listener = BflParentComment.new(@bfl_user_id, @mock_tweeter)
+      comment_listener.expects(:post_as_bot).with(GroupMe::KRYPPIE_BOT_ID, stub_url)
+
       assert_nothing_raised do
-        BflParentComment.new(@bfl_user_id, @mock_tweeter).tweet(message)
+        comment_listener.tweet(stub_message)
+      end
+    end
+
+    def test_twitter_error
+      stub_message = { "user_id" => @bfl_user_id, "text" => "as a parent" }
+      error = TwitterSupport::Error.new("boom")
+      response_message = "This should be tweeted, 'as a parent', but I couldn't do it."
+      @mock_tweeter.expects(:tweet).with("as a parent").once.raises(error)
+      comment_listener = BflParentComment.new(@bfl_user_id, @mock_tweeter)
+      comment_listener.expects(:post_as_bot).with(GroupMe::KRYPPIE_BOT_ID, response_message)
+
+      assert_nothing_raised do
+        comment_listener.tweet(stub_message)
       end
     end
 

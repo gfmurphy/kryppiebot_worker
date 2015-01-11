1c2864eb6d71815b2846b740574814f2bb782685
George F. Murphy <gfmurphy@gmail.com>
2015-01-10 18:25:35 -0600

    Add Congrats message listener.
    This listener tries to dogpile on congrats that are extended to a
    member on the group.

--- lib/listeners.rb
+++ lib/listeners.rb
@@ -1,4 +1,5 @@
 require "listeners/bfl_parent_comment"
+require "listeners/congrats"
 require "logging"
 require "redis"
 require "user_recent_messages"
@@ -10,7 +11,8 @@ module Listeners
     [
       ->(msg) { Logging.log(:info).message(msg.inspect) },
       ->(msg) { UserRecentMessages.new(msg["user_id"], Redis.new(url: REDIS_URL)).add msg },
-      ->(msg) { BflParentComment.new(ENV["BFL_USER_ID"], ShitBflSays.new(ENV["SBFL_SAYS_TOKEN"], ENV["SBFL_SAYS_SECRET"])).tweet(msg) }
+      ->(msg) { BflParentComment.new(ENV["BFL_USER_ID"], ShitBflSays.new(ENV["SBFL_SAYS_TOKEN"], ENV["SBFL_SAYS_SECRET"])).tweet(msg) },
+      ->(msg) { Congrats.new(Redis.new(url: REDIS_URL)).listen(msg) }
     ]
   end
 end
---
+++ lib/listeners/congrats.rb
@@ -0,0 +1,93 @@
+require "group_me"
+
+module Listeners
+  class Congrats
+    include GroupMe
+
+    def initialize(redis)
+      @redis = redis
+      @image_set = ImageSet.new(ImageSet::CONGRATS, @redis)
+    end
+
+    def listen(message)
+      text = message["text"]
+      resp = CongratsMessage.new(text).response
+      if resp.congrats? && !recent_response?(resp.name)
+        post_as_bot(KRYPPIE_BOT_ID, resp.text, @image_set.random)
+        record_response(resp.name)
+      end
+    end
+
+    private
+    def recent_response?(name)
+      @redis.get(response_key(name))
+    end
+
+    def record_response(name)
+      key = response_key(name)
+      @redis.multi do
+        @redis.set(key, "1")
+        @redis.expires(key, 3600)
+      end
+    end
+
+    def response_key(name)
+      [ImageSet::CONGRATS, 'responses', name].compact.join(":")
+    end
+
+    class CongratsMessage
+      def self.possible_responses
+        ['Mazel tov', 'Cheers', 'Congrats', 'gz', 'Gratz', 'Hip Hip', 
+         'Felicitations', 'Kudos', 'Props', 'Hats off']
+      end
+
+      def initialize(message)
+        @message = message.to_s
+      end
+
+      def response
+        CongratsResponse.new(*parse_message)
+      end
+
+      def parse_message
+        match = @message.match(message_pattern)
+        if match
+          [select_response, match_name(match[2])]
+        else
+          []
+        end
+      end
+
+      private
+      def match_name(name)
+        return name if name.nil?
+        name.gsub(/\W/, '').capitalize
+      end
+
+      def message_pattern
+        /\b(congrats|mazel tov|cheers|gratz|kudos|props)(,\s+\w+)?\b/i
+      end
+
+      def select_response
+        CongratsMessage.possible_responses.sample
+      end
+    end
+      
+    class CongratsResponse
+      attr_reader :name
+
+      def initialize(response, name=nil)
+        @text = response
+        @name = name
+      end
+
+      def text
+        [@text, @name].compact.join(", ").tap { |t| t << "!" unless t.empty? }
+      end
+
+      def congrats?
+        !text.empty?
+      end
+    end
+  end
+end
